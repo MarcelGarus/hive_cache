d70fe96ad2e0a7e8d8a1da65a1dc97aa867920b6
Marcel Garus <marcel.garus@gmail.com>
2020-03-31 16:23:29 +0200

    use ScopedBuilder from flutter_cached

--- lib/src/builders.dart
+++ lib/src/builders.dart
@@ -1,55 +1,5 @@
 part of 'cache.dart';
 
-typedef FetchCallback = Future<void> Function({bool force});
-
-typedef RawBuilder<T> = Widget Function(
-  BuildContext,
-  StreamAndData<T, CachedFetchStreamData<dynamic>>,
-);
-typedef FetchableBuilder<T> = Widget Function(
-  BuildContext,
-  T,
-  FetchCallback fetch,
-);
-
-class _ScopedBuilder<T> extends StatefulWidget {
-  const _ScopedBuilder({
-    Key key,
-    @required this.create,
-    @required this.destroy,
-    @required this.builder,
-  })  : assert(create != null),
-        assert(destroy != null),
-        assert(builder != null),
-        super(key: key);
-
-  final T Function() create;
-  final void Function(T) destroy;
-  final Widget Function(BuildContext, T) builder;
-
-  @override
-  State<StatefulWidget> createState() => _ScopedBuilderState<T>();
-}
-
-class _ScopedBuilderState<T> extends State<_ScopedBuilder<T>> {
-  T object;
-
-  @override
-  void initState() {
-    super.initState();
-    object = widget.create();
-  }
-
-  @override
-  void dispose() {
-    widget.destroy(object);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) => widget.builder(context, object);
-}
-
 class EntityBuilder<E extends Entity<E>> extends StatelessWidget {
   const EntityBuilder({
     Key key,
@@ -57,31 +7,29 @@ class EntityBuilder<E extends Entity<E>> extends StatelessWidget {
     @required this.builder,
   }) : super(key: key);
 
-  static _ScopedBuilder raw<E extends Entity<E>>({
+  static ScopedBuilder raw<E extends Entity<E>>({
     Key key,
     @required Id<E> id,
     @required RawBuilder<E> builder,
   }) =>
-      _ScopedBuilder<StreamAndData<E, CachedFetchStreamData<dynamic>>>(
+      ScopedBuilder<StreamAndData<E, CachedFetchStreamData<dynamic>>>(
         create: () => id.resolve(),
         destroy: (stream) => stream.dispose(),
         builder: builder,
       );
 
   final Id<E> id;
-  final FetchableBuilder<AsyncSnapshot<E>> builder;
+  final FetchableBuilder<CacheSnapshot<E>> builder;
 
   @override
   Widget build(BuildContext context) {
     return EntityBuilder.raw<E>(
       id: id,
-      builder: (_, stream) => StreamBuilder<E>(
-        stream: stream,
-        builder: (context, snapshot) => builder(
-          context,
-          snapshot,
-          stream.fetch,
+      builder: (_, stream) => CachedBuilder<E>(
+        stream: stream.handleError(
+          (error, stackTrace) => throw ErrorAndStacktrace(error, stackTrace),
         ),
+        builder: builder,
       ),
     );
   }
@@ -95,21 +43,17 @@ class EntityListBuilder<E extends Entity<E>> extends StatelessWidget {
   }) : super(key: key);
 
   final List<Id<E>> ids;
-  final FetchableBuilder<AsyncSnapshot<List<E>>> builder;
+  final FetchableBuilder<CacheSnapshot<List<E>>> builder;
 
   @override
   Widget build(BuildContext context) {
-    return _ScopedBuilder<
+    return ScopedBuilder<
         StreamAndData<List<E>, CachedFetchStreamData<dynamic>>>(
       create: () => ids.resolveAll(),
       destroy: (stream) => stream.dispose(),
-      builder: (_, stream) => StreamBuilder<List<E>>(
+      builder: (_, stream) => CachedBuilder<List<E>>(
         stream: stream,
-        builder: (context, snapshot) => builder(
-          context,
-          snapshot,
-          stream.fetch,
-        ),
+        builder: builder,
       ),
     );
   }
@@ -127,27 +71,23 @@ class CollectionBuilder<E extends Entity<E>> extends StatelessWidget {
   static _PopulatedCollectionBuilder<E> populated<E extends Entity<E>>({
     Key key,
     @required Collection<E> collection,
-    @required FetchableBuilder<AsyncSnapshot<List<E>>> builder,
+    @required FetchableBuilder<CacheSnapshot<List<E>>> builder,
   }) =>
       _PopulatedCollectionBuilder(
           key: key, collection: collection, builder: builder);
 
   final Collection<E> collection;
-  final FetchableBuilder<AsyncSnapshot<List<Id<E>>>> builder;
+  final FetchableBuilder<CacheSnapshot<List<Id<E>>>> builder;
 
   @override
   Widget build(BuildContext context) {
-    return _ScopedBuilder<
+    return ScopedBuilder<
         StreamAndData<List<Id<E>>, CachedFetchStreamData<dynamic>>>(
       create: () => collection.resolve(),
       destroy: (stream) => stream.dispose(),
-      builder: (_, streamOfIds) => StreamBuilder<List<Id<E>>>(
+      builder: (_, streamOfIds) => CachedBuilder<List<Id<E>>>(
         stream: streamOfIds,
-        builder: (context, snapshot) => builder(
-          context,
-          snapshot,
-          streamOfIds.fetch,
-        ),
+        builder: builder,
       ),
     );
   }
@@ -163,21 +103,17 @@ class _PopulatedCollectionBuilder<E extends Entity<E>> extends StatelessWidget {
         super(key: key);
 
   final Collection<E> collection;
-  final FetchableBuilder<AsyncSnapshot<List<E>>> builder;
+  final FetchableBuilder<CacheSnapshot<List<E>>> builder;
 
   @override
   Widget build(BuildContext context) {
-    return _ScopedBuilder<
+    return ScopedBuilder<
         StreamAndData<List<E>, CachedFetchStreamData<dynamic>>>(
       create: () => collection.resolve().resolveAll(),
       destroy: (stream) => stream.dispose(),
-      builder: (_, stream) => StreamBuilder<List<E>>(
+      builder: (_, stream) => CachedBuilder<List<E>>(
         stream: stream,
-        builder: (context, snapshot) => builder(
-          context,
-          snapshot,
-          stream.fetch,
-        ),
+        builder: builder,
       ),
     );
   }
@@ -195,26 +131,22 @@ class ConnectionBuilder<E extends Entity<E>> extends StatelessWidget {
   static _PopulatedConnectionBuilder<E> populated<E extends Entity<E>>({
     Key key,
     @required Connection<E> connection,
-    @required FetchableBuilder<AsyncSnapshot<E>> builder,
+    @required FetchableBuilder<CacheSnapshot<E>> builder,
   }) =>
       _PopulatedConnectionBuilder(
           key: key, connection: connection, builder: builder);
 
   final Connection<E> connection;
-  final FetchableBuilder<AsyncSnapshot<Id<E>>> builder;
+  final FetchableBuilder<CacheSnapshot<Id<E>>> builder;
 
   @override
   Widget build(BuildContext context) {
-    return _ScopedBuilder<StreamAndData<Id<E>, CachedFetchStreamData<dynamic>>>(
+    return ScopedBuilder<StreamAndData<Id<E>, CachedFetchStreamData<dynamic>>>(
       create: () => connection.resolve(),
       destroy: (stream) => stream.dispose(),
-      builder: (_, stream) => StreamBuilder<Id<E>>(
+      builder: (_, stream) => CachedBuilder<Id<E>>(
         stream: stream,
-        builder: (context, snapshot) => builder(
-          context,
-          snapshot,
-          stream.fetch,
-        ),
+        builder: builder,
       ),
     );
   }
@@ -230,20 +162,16 @@ class _PopulatedConnectionBuilder<E extends Entity<E>> extends StatelessWidget {
         super(key: key);
 
   final Connection<E> connection;
-  final FetchableBuilder<AsyncSnapshot<E>> builder;
+  final FetchableBuilder<CacheSnapshot<E>> builder;
 
   @override
   Widget build(BuildContext context) {
-    return _ScopedBuilder<StreamAndData<E, CachedFetchStreamData<dynamic>>>(
+    return ScopedBuilder<StreamAndData<E, CachedFetchStreamData<dynamic>>>(
       create: () => connection.resolve().resolve(),
       destroy: (stream) => stream.dispose(),
-      builder: (_, stream) => StreamBuilder<E>(
+      builder: (_, stream) => CachedBuilder<E>(
         stream: stream,
-        builder: (context, snapshot) => builder(
-          context,
-          snapshot,
-          stream.fetch,
-        ),
+        builder: builder,
       ),
     );
   }
